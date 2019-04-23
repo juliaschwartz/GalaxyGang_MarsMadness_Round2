33b8fe2f1d077c0b7d01d6fcfcc84542cfb5c07c
staceysamedi <44531977+staceysamedi@users.noreply.github.com>
2019-04-23 05:47:57 -0400

    Update local_multiplayer_game.java
    - included code to shuffle the front of the cards when the game is run
    - set P2 text to start as gray (inactivity)

--- app/src/main/java/com/example/galaxygang_marsmadness_round2/local_multiplayer_game.java
+++ app/src/main/java/com/example/galaxygang_marsmadness_round2/local_multiplayer_game.java
@@ -51,6 +51,49 @@ public class local_multiplayer_game extends AppCompatActivity {
         iv_43 = (ImageView) findViewById(R.id.iv_43);
         iv_44 = (ImageView) findViewById(R.id.iv_44);
 
+        iv_11.setTag("0");
+        iv_12.setTag("1");
+        iv_13.setTag("2");
+        iv_14.setTag("3");
+        iv_21.setTag("4");
+        iv_22.setTag("5");
+        iv_23.setTag("6");
+        iv_24.setTag("7");
+        iv_31.setTag("8");
+        iv_32.setTag("9");
+        iv_33.setTag("10");
+        iv_34.setTag(11);
+        iv_41.setTag("12");
+        iv_42.setTag("13");
+        iv_43.setTag("14");
+        iv_44.setTag("15");
 
+        //load the card images
+        frontOfCardResources();
+
+        //shuffle the images
+        Collections.shuffle(Arrays.asList());
+
+        //changing the color of the second player to show inactivity
+        tv_p2.setTextColor(Color.GRAY);
+    }
+
+    private void frontOfCardResources(){
+        image101 = R.drawable.ic_image101;
+        image102 = R.drawable.ic_image102;
+        image103 = R.drawable.ic_image103;
+        image104 = R.drawable.ic_image104;
+        image105 = R.drawable.ic_image105;
+        image106 = R.drawable.ic_image106;
+        image107 = R.drawable.ic_image107;
+        image108 = R.drawable.ic_image108;
+        image201 = R.drawable.ic_image201;
+        image202 = R.drawable.ic_image202;
+        image203 = R.drawable.ic_image203;
+        image204 = R.drawable.ic_image204;
+        image205 = R.drawable.ic_image205;
+        image206 = R.drawable.ic_image206;
+        image207 = R.drawable.ic_image207;
+        image208 = R.drawable.ic_image208;
     }
 }
