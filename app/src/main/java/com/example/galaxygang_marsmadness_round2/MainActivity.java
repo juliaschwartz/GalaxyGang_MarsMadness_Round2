274aeeb25a721ae4eb93a1491d3fed0dae0660b6
staceysamedi <44531977+staceysamedi@users.noreply.github.com>
2019-04-23 04:56:29 -0400

    Update on MainActivity,java
    connected the image views and text views for multiplayer game to java file

--- app/src/main/java/com/example/galaxygang_marsmadness_round2/MainActivity.java
+++ app/src/main/java/com/example/galaxygang_marsmadness_round2/MainActivity.java
@@ -5,9 +5,35 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_p1, tv_p2;
+
+    ImageView iv_11, iv_12, iv_13, iv_14,iv_21, iv_22, iv_23, iv_24, iv_31, iv_32, iv_33, iv_34, iv_41, iv_42, iv_43, iv_44;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_p1 = (TextView) findViewById(R.id.tv_p1);
+        tv_p2 = (TextView) findViewById(R.id.tv_p2);
+
+        iv_11 = (ImageView) findViewById(R.id.iv_11);
+        iv_12 = (ImageView) findViewById(R.id.iv_12);
+        iv_13 = (ImageView) findViewById(R.id.iv_13);
+        iv_14 = (ImageView) findViewById(R.id.iv_14);
+        iv_21 = (ImageView) findViewById(R.id.iv_21);
+        iv_22 = (ImageView) findViewById(R.id.iv_22);
+        iv_23 = (ImageView) findViewById(R.id.iv_23);
+        iv_24 = (ImageView) findViewById(R.id.iv_24);
+        iv_31 = (ImageView) findViewById(R.id.iv_31);
+        iv_32 = (ImageView) findViewById(R.id.iv_32);
+        iv_33 = (ImageView) findViewById(R.id.iv_33);
+        iv_34 = (ImageView) findViewById(R.id.iv_34);
+        iv_41 = (ImageView) findViewById(R.id.iv_41);
+        iv_42 = (ImageView) findViewById(R.id.iv_42);
+        iv_43 = (ImageView) findViewById(R.id.iv_43);
+        iv_44 = (ImageView) findViewById(R.id.iv_44);
+
+
     }
 }
