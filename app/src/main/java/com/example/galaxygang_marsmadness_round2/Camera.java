1db85b29c97177f66b9251ab6c38fa0fb37431ee
julia <juliastz@bu.edu>
2019-04-30 00:30:26 -0400

    up to FileProvider, good tidbit about it on stackoverflow

--- app/src/main/java/com/example/galaxygang_marsmadness_round2/Camera.java
+++ app/src/main/java/com/example/galaxygang_marsmadness_round2/Camera.java
@@ -6,6 +6,12 @@ import android.graphics.Bitmap;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.net.Uri;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.os.Environment;
 
 
 public class Camera extends Activity{
@@ -37,4 +43,55 @@ public class Camera extends Activity{
             image.setImageBitmap(imageBitmap);
         }
     }
+
+    //create a collision resistant file name using a date time stamp
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name based off of the timestamp
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    //Above is a method that creates a file name for the photo based off of the timestamp
+    //Next step: create & invoke the intent
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+                System.out.println("Error occurred while creating the picture file!");
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
 }
