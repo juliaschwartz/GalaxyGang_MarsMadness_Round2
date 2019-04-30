2ae2800512ea8ba1daf07a6a2ee6f7fc2f34817f
r-mic <49769873+r-mic@users.noreply.github.com>
2019-04-30 08:30:12 -0400

    Adding camera component

--- app/src/main/java/com/example/galaxygang_marsmadness_round2/Camera.java
+++ app/src/main/java/com/example/galaxygang_marsmadness_round2/Camera.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import android.os.Environment;
 
 
+
 public class Camera extends Activity{
     //extends activity allows you to use instances of app.Activity from java import
 
---
+++ app/src/main/java/com/example/galaxygang_marsmadness_round2/camera2.java
@@ -0,0 +1,12 @@
+package com.example.galaxygang_marsmadness_round2;
+
+public class camera2 {
+
+
+}
+
+//Check to see if the device has a camera:
+private boolean checkCameraHardware(Context)
+
+//Don't forget Camera.release();
+
