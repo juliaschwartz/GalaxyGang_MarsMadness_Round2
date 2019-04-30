0b89716647e54adadb856573210737d7cbe4810c
julia <juliastz@bu.edu>
2019-04-30 17:31:06 -0400

    just the thumbnail

--- app/src/main/java/com/example/galaxygang_marsmadness_round2/Camera.java
+++ app/src/main/java/com/example/galaxygang_marsmadness_round2/Camera.java
@@ -20,16 +20,16 @@ public class Camera extends Activity{
     ImageView image;
 
     //function that invoke an intent to capture a photo
-  //  static final int REQUEST_IMAGE_CAPTURE = 1;
-  //  private void dispatchTakePictureIntent() {
-  //      Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-  //      if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+     static final int REQUEST_IMAGE_CAPTURE = 1;
+     private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // returns the first activity component that can handle the intent.
             // if you start the startActivityForResult function without intent, the
             // app will crash.
-    //        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-   //     }
-   // }
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+       }
+    }
 
 
     //Do something with the photo!
@@ -39,59 +39,65 @@ public class Camera extends Activity{
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             //the photo is stored in the extras under the key "data", so retrieve that
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            image.setImageBitmap(imageBitmap);
+            //if(extras != null) { //if the thumbnail is null, the function failed, it exits
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                image.setImageBitmap(imageBitmap);
+            //}
         }
     }
 
-    //create a collision resistant file name using a date time stamp
-    String currentPhotoPath;
 
-    private File createImageFile() throws IOException {
+
+
+
+
+
+    //create a collision resistant file name using a date time stamp
+    //String currentPhotoPath;
+
+    //private File createImageFile() throws IOException {
         // Create an image file name based off of the timestamp
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
+       // String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        //String imageFileName = "JPEG_" + timeStamp + "_";
+       // File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //File image = File.createTempFile(
+        //        imageFileName,  /* prefix */
+         //       ".jpg",         /* suffix */
+          //      storageDir      /* directory */
+        //);
 
         // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
+        //currentPhotoPath = image.getAbsolutePath();
+        //return image;
+  //  }
 
     //Above is a method that creates a file name for the photo based off of the timestamp
     //Next step: create & invoke the intent
 
-    static final int REQUEST_TAKE_PHOTO = 1;
+    //static final int REQUEST_TAKE_PHOTO = 1;
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+   // private void dispatchTakePictureIntent() {
+      //  Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+    //    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
+     //       File photoFile = null;
+       //     try {
+         //       photoFile = createImageFile();
+           // } catch (IOException ex) {
                 // Error occurred while creating the File
 
-                System.out.println("Error occurred while creating the picture file!");
+             //   System.out.println("Error occurred while creating the picture file!");
 
-            }
+          //  }
             // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
-
-
+            //if (photoFile != null) {
+              //  Uri photoURI = FileProvider.getUriForFile(this,
+                //        "com.example.android.fileprovider",
+                  //      photoFile);
+                //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                //startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+          //  }
+       // }
+    //}
 }
